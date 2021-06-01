5a7db2ce0301c32312edf2303d026718378ca4eb
boutouditsaid@gmail.com <boutouditsaid@gmail.com>
2021-06-01 14:20:50 +0100

    template integration

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,5 @@
+@include('admin.layouts.header')
+@include('admin.layouts.sidebare')
+<div class="main-content">
+    @yield('content')
+@include('admin.layouts.footer')
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/test', function () {
+    return view('test');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
