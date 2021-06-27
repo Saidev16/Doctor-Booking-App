3ab77509444ae9fae6c16cfae41f24d883d9335d
kamal ait omar <aitomarkamal@gmail.com>
2021-06-27 13:54:52 +0100

    prescription form (still not working)

--- resources/views/prescription/index.blade.php
+++ resources/views/prescription/index.blade.php
@@ -40,7 +40,11 @@
                                         @if ($booking->status == 0)
                                             <a href="{{route('update.status',[$booking->id])}}"> <button class="btn btn-primary">En Attente</button> </a> 
                                         @else
-                                            <a href="{{route('update.status',[$booking->id])}}"><button class="btn btn-success">Confirmé</button></a> 
+                                            <a href="{{route('update.status',[$booking->id])}}"><button class="btn btn-success">Confirmé</button></a>
+                                            <!-- Button trigger modal -->
+                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kml">
+                                                write perscription
+                                            </button> 
                                         @endif
                                     </td>
                                 </tr>
@@ -53,6 +57,26 @@
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="kml" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        ...
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+    </div>
     </div>
 </div>
 @endsection
